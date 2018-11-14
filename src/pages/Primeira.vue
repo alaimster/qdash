e2ee029fa81a6dc70a3b2541c7e6820369b6ec43
alaimster <alaimster@gmail.com>
2018-11-14 00:54:22 -0200

    wip

--- src/pages/Primeira.vue
+++ src/pages/Primeira.vue
@@ -19,31 +19,52 @@
           </div>
           <div class="row gutter-md">
             <div class="col-xs-12 col-sm-6">
-              <q-search no-icon :after="[{icon: 'icon-la la la-map-marker', handler () {}}]" inverted v-model="from" float-label="Sair de" placeholder="Cidade ou Aeroporto">
+              <q-search no-icon :after="[{icon: 'icon-la la la-map-marker', handler () {}}]" ref="from" inverted v-model="from" placeholder="De onde?">
                 <q-autocomplete
                   :value-field="v => `${ v.label } (${ v.value })`"
                   @search="search"
+                  @selected="$refs.to.focus()"
                 />
               </q-search>
             </div>
             <div class="col-xs-12 col-sm-6">
-              <q-search no-icon :after="[{icon: 'icon-la la la-map-marker', handler () {}}]" inverted v-model="to" float-label="Ir para" placeholder="Cidade ou Aeroporto">
+              <q-search no-icon :after="[{icon: 'icon-la la la-map-marker', handler () {}}]" ref="to" inverted v-model="to" placeholder="Para onde?">
                 <q-autocomplete
                   :value-field="v => `${ v.label } (${ v.value })`"
                   @search="search"
+                  @selected="$refs.departure_date.$el.click()"
                 />
               </q-search>
             </div>
           </div>
           <div class="row gutter-md q-pt-md">
             <div v-bind:class="[trip_type === 'OW' ? 'col-sm-6' : 'col-sm-4']" class="col-xs-12">
-              <q-datetime :min="today" format="DD-MM-YYYY" inverted v-model="departure_date" :after="[{icon: 'icon-la la la-calendar', handler () {}}]" float-label="Partida" />
+              <q-datetime
+                :min="today"
+                :max="return_date"
+                format="DD/MM/YYYY"
+                inverted
+                v-model="departure_date"
+                ref="departure_date"
+                :after="[{icon: 'icon-la la la-calendar', handler () {}}]"
+                @change="trip_type === 'RT' ? $refs.return_date.$el.click() : $refs.submit.$el.focus()"
+                placeholder="Data de Partida"
+              />
             </div>
             <div v-show="trip_type === 'RT'" class="col-xs-12 col-sm-4">
-              <q-datetime :min="departure_date || today" format="DD-MM-YYYY" inverted v-model="return_date" :after="[{icon: 'icon-la la la-calendar', handler () {}}]" float-label="Retorno" />
+              <q-datetime
+                :min="departure_date || today"
+                format="DD/MM/YYYY"
+                inverted
+                v-model="return_date"
+                ref="return_date"
+                :after="[{icon: 'icon-la la la-calendar', handler () {}}]"
+                @change="$refs.submit.$el.focus()"
+                placeholder="Data de Volta"
+              />
             </div>
             <div v-bind:class="[trip_type === 'OW' ? 'col-sm-6' : 'col-sm-4']" class="col-xs-12">
-              <q-btn color="primary" @click="send" no-caps id="trip-submit" class="full-width full-height" label="PESQUISAR PASSAGEM" />
+              <q-btn color="primary" @click="send" ref="submit" no-caps id="trip-submit" class="full-width full-height" label="PESQUISAR PASSAGEM" />
             </div>
           </div>
         </div>
@@ -96,6 +117,23 @@ export default {
 <style lang="stylus">
   @import './../css/themes/common.variables.styl'
 
+  @keyframes framed
+    0%
+        left: 0%;
+        opacity: 0
+    5%
+        opacity: 0
+    10%
+        opacity: .12
+    60%
+        opacity: .1
+    70%
+        opacity: .08
+    85%
+        opacity: 0;
+    to
+        left: 90%
+
   #page-block
     margin: 10px 35px
     background $secondary
@@ -105,9 +143,24 @@ export default {
       border-radius 4px
       box-shadow none
       font-weight:400
-      font-size 18px
+      font-size 16px
       box-shadow 3px 3px 0px 0px $secondary_dark
       text-shadow: 1px 1px 0px $secondary_dark;
+      overflow hidden
+      &:after
+        content: "";
+        position: absolute;
+        opacity: 0;
+        top: 0;
+        left: 0;
+        background: linear-gradient(90deg,hsla(0,0%,100%,0) 0,hsla(0,0%,100%,.03) 1%,hsla(0,0%,100%,.6) 30%,hsla(0,0%,100%,.85) 50%,hsla(0,0%,100%,.85) 60%,hsla(0,0%,100%,.85) 0,hsla(0,0%,100%,0));
+        width: 25%;
+        height: 100%;
+        transform: skew(-10deg,0deg);
+        animation: framed 3s;
+        animation-iteration-count: infinite;
+        animation-delay: 1s;
+
     .q-option-label
       color $secondary_ultra_light
       font-weight 400
@@ -115,6 +168,7 @@ export default {
       background $secondary_light !important
       color $secondary_ultra_light !important
       box-shadow none
+      min-height: 50px;
       border-radius 4px
       font-weight 400
       border 2px solid transparent
@@ -130,7 +184,7 @@ export default {
         font-weight 500
         line-height 26px
       .q-if-control
-        font-size 28px !important
+        font-size 25px !important
         color $primary
         &:nth-of-type(2)
           display none
